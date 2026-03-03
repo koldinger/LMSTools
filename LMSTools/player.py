036bcd55bab8bf7bcb985433efa519339082698d
Eric Koldinger <kolding@washington.edu>
2026-03-03 10:33:10 -0800

    Converted to Python3 with 2to3

--- LMSTools/player.py
+++ LMSTools/player.py
@@ -4,8 +4,8 @@
 #
 # This set of tools was inspired by the PyLMS library.
 
-from tags import LMSTags as tags
-from utils import LMSUtils
+from .tags import LMSTags as tags
+from .utils import LMSUtils
 
 
 DETAILED_TAGS = [tags.ARTIST,
@@ -140,7 +140,7 @@ class LMSPlayer(LMSUtils):
         """Play/Pause Toggle"""
         self.request("pause")
 
-    def next(self):
+    def __next__(self):
         """Play next item in playlist"""
         self.request("playlist jump +1")
 
